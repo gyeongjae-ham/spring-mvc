0fa90fb007fd1ef85612047a557e55e67efb7c9c
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-05-02 10:26:58 +0900

    :sparkles: Add Member, MemberRepository, methods and test

---
+++ src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,51 @@
+package hello.servlet.domain.member;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MemberRepositoryTest {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    // 테스트 순서가 보장이 안되기 때문에 테스트간에 영향을 주지 않기 위해서
+    // 테스트마다 종료 후 메모리를 지워준다
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        //given
+        Member member = new Member("hello", 20);
+
+        //when
+        Member saveMember = memberRepository.save(member);
+
+        //then
+        Member findMember = memberRepository.findById(saveMember.getId());
+
+        assertThat(findMember).isEqualTo(saveMember);
+    }
+
+    @Test
+    void findAll() {
+        //given
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 30);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        //when
+        List<Member> result = memberRepository.findAll();
+
+        //then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(member1, member2);
+    }
+}
