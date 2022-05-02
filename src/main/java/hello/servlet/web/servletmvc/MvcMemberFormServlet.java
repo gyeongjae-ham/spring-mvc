293b779aa5144133a9b336e00d7361abf552041d
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-05-02 21:23:31 +0900

    :sparkles: Build member domain with ServletMVC

---
+++ src/main/java/hello/servlet/web/servletmvc/MvcMemberFormServlet.java
@@ -0,0 +1,22 @@
+package hello.servlet.web.servletmvc;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "mvcMemberFormServlet", urlPatterns = "/servlet-mvc/members/new-form")
+public class MvcMemberFormServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String viewPath = "/WEB-INF/views/new-form.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        // 다른 서블릿이나 JSP로 이동할 수 있는 기능이다(리다이렉트 아님), 서버 내부에서 다시 호출이 발생하는 것
+        // WEB-INF 밑에 있는 것은 항상 Controller를 거치게 된다(그냥 WAS에 정해진 룰)
+        dispatcher.forward(request, response);
+    }
+}
