78e038f072c816ff91489426259ea2426cbee75c
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-05-04 14:50:21 +0900

    :sparkles: Adapter를 통해서 여러 버전의 Controller를 사용하기

---
+++ src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -0,0 +1,85 @@
+package hello.servlet.web.frontcontroller.v5;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import hello.servlet.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServletV5", urlPatterns = "/front-controller/v5/*")
+public class FrontControllerServletV5 extends HttpServlet {
+
+    // 기존에는 Object 위치에 특정한 controller의 버전이(인터페이스)가 들어갔는데 지금은 모든 버전의 controller를 다뤄야 하므로
+    // Object가 들어가 있다
+    private final Map<String, Object> handlerMappingMap = new HashMap<>();
+    // Adapter들을 담아둘 List
+    private final List<MyHandlerAdapter> handlerAdapters = new ArrayList<>();
+
+    public FrontControllerServletV5() {
+        initHandlerMappingMap(); // Controller의 경로별로 Mapping한 Map
+        initHandlerAdapters(); // Controller 버전에 맞는 Adapter들이 담겨 있는 List
+    }
+
+    private void initHandlerMappingMap() {
+        handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+    }
+
+    private void initHandlerAdapters() {
+        handlerAdapters.add(new ControllerV3HandlerAdapter());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // handlerMapping 정보를 뒤져서 해당하는 handler를 가져오고
+        Object handler = getHandler(request);
+        if (handler == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+        // 가져온 handler를 getHandlerAdapter에 던져서 해당 handler의 어댑터를 가져온다
+        MyHandlerAdapter adapter = getHandlerAdapter(handler);
+        // 해당 handler에 일치하는 Controller로 바꾼 후에 process를 진행시켜서 model, view를 가져온다
+        ModelView mv = adapter.handle(request, response, handler);
+
+        String viewName = mv.getViewName();
+        MyView view = viewResolver(viewName);
+
+        view.render(mv.getModel(), request, response);
+    }
+
+    private MyHandlerAdapter getHandlerAdapter(Object handler) {
+        // for문을 돌면서 handlerAdapters 리스트 안에서 해당 Controller에 해당하는 Adapter를 찾는다
+        for (MyHandlerAdapter adapter : handlerAdapters) {
+            if (adapter.supports(handler)) {
+                return adapter;
+            }
+        }
+        throw new IllegalArgumentException("handler adapter를 찾을 수 없습니다. handler= " + handler);
+    }
+
+    private Object getHandler(HttpServletRequest request) {
+        // URI 정보를 가져오고
+        String requestURI = request.getRequestURI();
+        // 가져온 URI 정보로 해당하는 경로의 controller를 가져온다
+        return handlerMappingMap.get(requestURI);
+    }
+
+    private MyView viewResolver(String viewName) {
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
+    }
+}
